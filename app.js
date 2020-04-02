336d834f5117e7ba210f46f6c01d4d339c13ac89
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2020-04-02 11:30:54 +0300

    update deploy 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (_, res) => {
-  res.send('<h1>My Node App</h1>')
+  res.send('<h1>My Node App and</h1>')
 });
 
 app.listen(5000, () => {
